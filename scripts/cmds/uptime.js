2f4cfe4ac90f03ce9582a9f96899c9a0fbcba008
ManashJoshi <156304424+ManashJoshi@users.noreply.github.com>
2024-06-17 19:57:07 +0545

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -7,7 +7,7 @@ module.exports = {
         name: "uptime",
         aliases: ["up", "upt"],
         version: "1.0",
-        author: "Itachi",
+        author: "Itachi",//-modified by dinesh
         role: 0,
         shortDescription: {
             en: "Displays the uptime of the bot."
@@ -21,7 +21,16 @@ module.exports = {
         }
     },
     onStart: async function ({ api, event, args }) {
-        const bold = 'https://i.imgur.com/k5py3Tj.mp4'; // Replace with your Google Drive videoid link
+        const videoUrls = [
+            'https://i.imgur.com/k5py3Tj.mp4',
+            'https://i.imgur.com/2Fz5gXT.mp4',
+            'https://i.imgur.com/JbVg4Ld.mp4'
+    ];
+
+        // Randomly select one video URL
+        const randomIndex = Math.floor(Math.random() * videoUrls.length);
+        const selectedVideoUrl = videoUrls[randomIndex];
+
         const tmpFolderPath = './tmp'; // Ensure this path exists or create it dynamically if needed
 
         // Ensure the tmp folder exists
@@ -30,7 +39,7 @@ module.exports = {
         }
 
         try {
-            const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+            const videoResponse = await axios.get(selectedVideoUrl, { responseType: 'arraybuffer' });
             const videoPath = path.join(tmpFolderPath, 'uptime_video.mp4');
             fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
 
@@ -42,7 +51,11 @@ module.exports = {
             const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
             const message = {
-                body: `Hello Master 😸\n\nYour Bot has been running for\n\n${uptimeString}.`,
+                body: `Hello Master 😸
+
+                                      Your Bot has been running for
+
+${uptimeString}.`,
                 attachment: fs.createReadStream(videoPath)
             };
 
