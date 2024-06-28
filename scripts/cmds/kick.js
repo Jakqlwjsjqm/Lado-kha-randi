2864759c4c5f8f8fdf17324415a24cf2ef0c0019
ManashJoshi <156304424+ManashJoshi@users.noreply.github.com>
2024-06-28 10:39:13 +0545

    Update kick.js

--- scripts/cmds/kick.js
+++ scripts/cmds/kick.js
@@ -1,11 +1,15 @@
 module.exports = {
 	config: {
 		name: "kick",
-		version: "1.3",
+		version: "1.2",
 		author: "NTKhang",
 		countDown: 5,
 		role: 1,
-		description: {
+		shortDescription: {
+			vi: "Kick thành viên",
+			en: "Kick member"
+		},
+		longDescription: {
 			vi: "Kick thành viên khỏi box chat",
 			en: "Kick member out of chat box"
 		},
@@ -29,21 +33,21 @@ module.exports = {
 		const adminIDs = await threadsData.get(event.threadID, "adminIDs");
 		if (!adminIDs.includes(api.getCurrentUserID()))
 			return message.reply(getLang("needAdmin"));
+
 		async function kickAndCheckError(uid) {
 			try {
 				await api.removeUserFromGroup(uid, event.threadID);
-			}
-			catch (e) {
+			} catch (e) {
 				message.reply(getLang("needAdmin"));
 				return "ERROR";
 			}
 		}
+
 		if (!args[0]) {
 			if (!event.messageReply)
 				return message.SyntaxError();
 			await kickAndCheckError(event.messageReply.senderID);
-		}
-		else {
+		} else {
 			const uids = Object.keys(event.mentions);
 			if (uids.length === 0)
 				return message.SyntaxError();
